4dc0fdf9549f417c8a39772e248fa2a409e7d173
SeongIkKim <kpic1638@gmail.com>
2023-03-24 16:14:55 -0400

    part 1

--- Project2/rdp_socket.py
+++ Project2/rdp_socket.py
@@ -1,15 +1,32 @@
 from utility import UnreliableSocket
 
+
 class RDTSocket(UnreliableSocket):
     _TIMEOUT_THRESHOLD = 500  # ms
 
     def accept(self):
         pass
+
     def connect(self):
+        """
+        Send START message and then wait for ACK.
+        seq_num of ACK message should be the same with that of START message.
+        After all, connection is open and can send additional packet messages.
+        :return:
+        """
         pass
+
     def send(self):
         pass
+
     def recv(self):
         pass
+
     def close(self):
+        """
+        Send END message and then wait for ACK.
+        seq_num of ACK message should be the same with that of END message.
+        After all, connection is closed.
+        :return:
+        """
         pass
--- Project2/utility.py
+++ Project2/utility.py
@@ -1,18 +1,47 @@
 from dataclasses import dataclass
+import socket
+from typing import Tuple, Any
+import zlib
 
 
-class UnreliableSocket:
-    def bind(self):
-        pass
+class UnreliableSocket(socket.socket):
+    def __init__(self):
+        super().__init__(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
 
-    def recvfrom(self):
-        pass
+    def bind(self, address: Tuple[str, int]) -> None:
+        """
+        Inherited from normal UDP socket
+        :param address: (host, port)
+        """
+        super().bind(address)
 
-    def sendto(self):
-        pass
+    def recvfrom(self, bufsize: int, flags: int = ...) -> Tuple[bytes, Any]:
+        """
+        Inherited from normal UDP socket
+        should simulate packet loss, packet delay, packet corruption scenarios
+        :param bufsize: receive buffer size
+        :param flags: flags
+        :return: datagram and return address
+        """
+        # TODO simulate packet loss
+        # TODO simulate packet delay
+        # TODO simulate packet corruption
+        return super().recvfrom(bufsize, flags)
 
-    def close(self):
-        pass
+    def sendto(self, data, address):
+        """
+        Inherited from normal UDP socket
+        :param data: readable buffer data
+        :param address: (host, port)
+        """
+        return super().sendto(data, address)
+
+    def close(self) -> None:
+        """
+        Inherited from normal UDP socket
+        :return:
+        """
+        self.close()
 
 
 @dataclass
@@ -23,8 +52,16 @@ class PacketHeader:
     checksum: int  # 32-bit crc
 
 
-def compute_checksum():
-    pass
+def compute_checksum(bin_str: bytes):
+    """
+    calculate the crc32 checksum value
+    :return:
+    """
+    return zlib.crc32(bin_str)
 
 def verify_packet():
+    """
+    verifies the integrity of the received segments
+    :return:
+    """
     pass
